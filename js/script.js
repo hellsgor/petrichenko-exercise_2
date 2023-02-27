08fe99de225e8e2f5a16f216c2416b61d1c5704f
hellsgor <hellsgor@gmail.com>
2023-02-27 22:53:10 +0400

    second exercise is done

--- js/script.js
+++ js/script.js
@@ -34,4 +34,60 @@ window.addEventListener('DOMContentLoaded', function() {
     }
   })
 
+
+  // TIMER
+
+  let year = new Date().getFullYear();
+  let month = `${new Date().getMonth() + 1}`.length === 1 ? `0${new Date().getMonth() + 1}` : `${new Date().getMonth() + 1}`;
+  let date = new Date().getDate() + 1;
+
+  let deadline = `${year}-${month}-${date}`;
+
+  function getTimeRemaining(endTime) {
+    let t = Date.parse(endTime) - Date.parse(new Date());
+    let seconds = Math.floor((t/1000) % 60);
+    let minutes = Math.floor((t / 1000 / 60) % 60);
+    let hours = Math.floor((t / 1000 / 60 / 60) % 24);
+
+    return {
+      'total': t,
+      'hours': hours,
+      'minute': minutes,
+      'seconds': seconds
+    }
+  }
+
+  function setClock(id, endTime) {
+    let timer = document.getElementById(id),
+      hours = timer.querySelector('.hours'),
+      minutes = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      let t = getTimeRemaining(endTime);
+      hours.textContent = t.hours;
+      minutes.textContent = t.minute;
+      seconds.textContent = t.seconds;
+      plusZero([hours, minutes, seconds]);
+
+      function plusZero(units) {
+        for (let i = 0; i < units.length; i++) {
+          if (units[i].textContent.length < 2) {
+            units[i].textContent = `0${units[i].textContent}`;
+          }
+        }
+      }
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+        let zeroTime = '00';
+        hours.textContent = zeroTime;
+        minutes.textContent = zeroTime;
+        seconds.textContent = zeroTime;
+      }
+    }
+  }
+
+  setClock('timer', deadline);
 })
